d89f2087c07430459476bb1bdd47350da587afa4
hhjeee <yoonhj0315@gmail.com>
2025-02-27 16:42:02 +0900

    [chore] rename variables, change type of instruction

---
+++ app/components/prompt/modals/InstructionItemModal.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import Modal from './Modal';
+import Button from '../Button';
+
+import { InstructionModalProps } from '~/types/modal';
+
+const InstructionItemModal = ({ isOpen, setIsOpen, item }: InstructionModalProps) => {
+  const [body, setBody] = useState('');
+
+  useEffect(() => {
+    if (item) {
+      setBody(item.body);
+    } else {
+      setBody('');
+    }
+  }, [item]);
+
+  const handleSave = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+      <div className="flex w-[40rem] flex-col gap-4">
+        <h2 className="text-lg font-semibold">Instruction Item</h2>
+
+        <div>
+          <label className="mb-1 block" htmlFor="body">
+            Body
+          </label>
+          <textarea
+            id="body"
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+            className="row-4 w-full rounded border p-2"
+            rows={8}
+            placeholder="body"
+          />
+        </div>
+
+        <div className="flex justify-end">
+          <Button text="Save" handleClick={handleSave} color="bg-green-500" />
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default InstructionItemModal;
--- app/routes/prompt.instruction.tsx
+++ app/routes/prompt.instruction.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import Button from '../components/prompt/Button';
-import InstructionModal from '~/components/prompt/modals/InstructionModal';
+import InstructionItemModal from '~/components/prompt/modals/InstructionItemModal';
 import Pagination from '~/components/prompt/Pagination';
 
 import { usePagination } from '~/hooks/usePagination';
@@ -46,16 +46,22 @@ const Instruction = () => {
       <table className="h-full">
         <thead>
           <tr className="text-gray-500">
-            <th className="py-2 text-start font-semibold">Title</th>
-            <th className="text-start font-semibold">Content</th>
+            <th className="py-2 text-start font-semibold">Name</th>
+            <th className="text-start font-semibold">Instruction Items</th>
             <th className="text-end font-semibold">Action</th>
           </tr>
         </thead>
         <tbody>
           {displayedItems.map((item) => (
             <tr key={item.id} className="border-y-2">
-              <td className="p-2 font-semibold">{item.title}</td>
-              <td className="w-2/3 whitespace-pre-line p-2">{item.content}</td>
+              <td className="p-2 font-semibold">{item.name}</td>
+              <td className="w-2/3 whitespace-pre-line p-2">
+                {item.instruction_items.map((innerItem) => (
+                  <p key={innerItem.id} className="mb-2">
+                    {innerItem.body}
+                  </p>
+                ))}
+              </td>
               <td className="flex h-full items-center justify-end gap-2">
                 <Button text="Edit" handleClick={() => {}} color="bg-green-500" />
                 <Button text="Delete" handleClick={() => {}} color="bg-red-500" />
@@ -109,7 +115,7 @@ const InstructionItem = () => {
       </table>
       <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
 
-      <InstructionModal isOpen={isOpen} setIsOpen={setIsOpen} item={editItem} />
+      <InstructionItemModal isOpen={isOpen} setIsOpen={setIsOpen} item={editItem} />
     </div>
   );
 };
@@ -117,16 +123,37 @@ const InstructionItem = () => {
 const INSTRUCTIONS = [
   {
     id: 1,
-    title: '문제구체화',
-    content: `<Instruction>
-      1. Reflecting on the previous conversation,  ask user to describe their current situation or challenge. Prompt the user to define their concern more specifically—what exactly is bothering them, and in what context does it arise?
-      2. Reassure the client and put their mind at ease. Comfort the client's heart in warm, empathic tone.
-      3. Paraphrase what the client says and empathize fully. If the user expresses a vague or broad situation, gently probe deeper by asking clarifying questions that help them reflect on what is truly causing their negative thoughts or feelings.
-      4. Guide the user in distinguishing between external factors (circumstances, other people) and internal factors (thoughts, biases, assumptions) contributing to their concern.
-      5. Identify the psychological factors underlying the client's superficial answers. Paraphrase what the client says and empathize fully.  
-      6. Maintain a neutral yet empathetic tone, ensuring the user feels understood and supported throughout the conversation.
-      7. Avoid giving direct advice or solutions unless the user explicitly asks for them. Your primary goal is to facilitate self-awareness, not problem-solving.
-      *Please make sure your answer does not exceed 5 sentences.`,
+    name: '문제구체화',
+    instruction_items: [
+      {
+        id: 1,
+        body: `Reflecting on the previous conversation, ask how they are feeling in a general sense. Use open-ended questions that encourage them to describe their emotions in their own words. Use warm tone and emphasize that the even if the client feels alone, you are on their side and client is not alone.`,
+      },
+      {
+        id: 2,
+        body: `Reassure the client and put their mind at ease. Comfort the client's heart in warm, empathic tone.`,
+      },
+      {
+        id: 3,
+        body: `Paraphrase what the client says and empathize fully. If the user expresses a vague or broad situation, gently probe deeper by asking clarifying questions that help them reflect on what is truly causing their negative thoughts or feelings.`,
+      },
+      {
+        id: 4,
+        body: `Guide the user in distinguishing between external factors (circumstances, other people) and internal factors (thoughts, biases, assumptions) contributing to their concern.`,
+      },
+      {
+        id: 5,
+        body: `Identify the psychological factors underlying the client's superficial answers. Paraphrase what the client says and empathize fully.`,
+      },
+      {
+        id: 6,
+        body: `Maintain a neutral yet empathetic tone, ensuring the user feels understood and supported throughout the conversation.`,
+      },
+      {
+        id: 7,
+        body: `Avoid giving direct advice or solutions unless the user explicitly asks for them. Your primary goal is to facilitate self-awareness, not problem-solving.`,
+      },
+    ],
   },
 ];
 
