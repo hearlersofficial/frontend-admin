aa0334da804c5496f9c9798cc7c274cee7b07b47
이승준 <sjlee@iseungjun-ui-MacBookPro.local>
2025-02-27 21:05:19 +0900

    feat(dashboard): dnd-kit 초기설정, 대시보드 컴포넌트 작성

---
+++ app/routes/dashboard/index.tsx
@@ -0,0 +1,39 @@
+import { useLoaderData } from "@remix-run/react";
+import { TechniqueContainer } from "./components/TechniqueContainer";
+import { useState } from "react";
+export async function loader() {
+  // TODO: 데이터 로딩
+  const techniqueCards = [
+    ["감정공감", "컨디션 물어보기", "상담 마무리", "오늘 일정 물어보기",],
+    ["감정공감", "뭐해"],
+  ]
+  return {techniqueCards};
+}
+
+export default function DashboardPage() {
+  const data = useLoaderData<typeof loader>();
+  const [initialCards, setInitialCards] = useState(data.techniqueCards);
+  
+  return (
+    <div>
+      <h1>Hearlers Admin</h1>
+      <div className="flex flex-col gap-3">
+        {initialCards.map((counsels, index) => (
+          <TechniqueContainer 
+            key={index} 
+            cards={counsels} 
+            setCards={(newCards) => {
+              const updatedCards = [...initialCards];
+              updatedCards[index] = newCards;
+              setInitialCards(updatedCards);
+            }}  
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export function HydrateFallback() {
+  return <div>Loading...</div>;
+}
