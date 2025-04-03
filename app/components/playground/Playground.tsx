9dc019e9db436b5ead7c5b6c4a0eb3fd52cddcad
hhjeee <yoonhj0315@gmail.com>
2025-04-03 21:49:25 +0900

    [feat] characters list

--- app/components/playground/Playground.tsx
+++ app/components/playground/Playground.tsx
@@ -1,4 +1,26 @@
-const Playground = () => {
-  return <>Playground</>;
-};
-export default Playground;
+import { useState } from 'react';
+
+import CharacterList from './CharacterList';
+
+const characters = [
+  { id: '1', name: '다혜', description: '해결' },
+  { id: '2', name: '다혜', description: '해결' },
+  { id: '3', name: '다혜', description: '해결' },
+  { id: '4', name: '다혜', description: '해결' },
+];
+
+export default function Playground() {
+  const [selectedCharacter, setSelectedCharacter] = useState(characters[0].id);
+
+  return (
+    <div className="flex pr-20">
+      <div className="w-[100px]">
+        <CharacterList characters={characters} selected={selectedCharacter} onSelect={setSelectedCharacter} />
+      </div>
+
+      <div
+        className={`flex flex-1 rounded-xl bg-white px-8 py-6 ${selectedCharacter === characters[0].id && 'rounded-tl-none'}`}
+      ></div>
+    </div>
+  );
+}
