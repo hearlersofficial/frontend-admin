1a83b855b7246c98610d7317ed48a0fa5efe406c
hhjeee <yoonhj0315@gmail.com>
2025-04-05 23:22:58 +0900

    [feat] save prompt modal

--- app/types/prompt.ts
+++ app/types/prompt.ts
@@ -1,1 +1,14 @@
 export type PromptType = 'Persona' | 'Context' | 'Instruction' | 'Tone';
+
+export interface Prompt {
+  id: number;
+  title: string;
+  time: string;
+  fav: boolean;
+  memo: string;
+}
+
+export interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+}
