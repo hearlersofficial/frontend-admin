b55fef741ce8f6bcdc3016881696165bd5074d5e
hhjeee <yoonhj0315@gmail.com>
2025-06-03 13:57:24 +0900

    [feat] inject env from root loader, header login tag

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,32 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { json, Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteLoaderData } from '@remix-run/react';
+import type { LinksFunction, LoaderFunction } from '@remix-run/node';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-import "./tailwind.css";
+import './tailwind.css';
+
+export const loader: LoaderFunction = async () => {
+  return json({
+    ENV: {
+      API_URL: process.env.API_URL,
+    },
+  });
+};
 
 export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
+    rel: 'preconnect',
+    href: 'https://fonts.gstatic.com',
+    crossOrigin: 'anonymous',
   },
   {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+    rel: 'stylesheet',
+    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
   },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const data = useRouteLoaderData<typeof loader>('root');
   return (
     <html lang="en">
       <head>
@@ -35,6 +38,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         {children}
         <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.ENV = ${JSON.stringify(data.ENV || {})};`,
+          }}
+        />
         <Scripts />
       </body>
     </html>
@@ -47,9 +55,10 @@ export default function App() {
     defaultOptions: {
       queries: {
         staleTime: 60 * 1000,
-      }
-    }
+      },
+    },
   });
+
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
