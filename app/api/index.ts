b96fb15c1795c32313bc0c7de6c1b431f472482e
hhjeee <yoonhj0315@gmail.com>
2025-06-12 15:34:51 +0900

    [feat] add credential

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,7 +1,14 @@
 import { V1 } from '~/__generated__/V1';
 
+const fetchWithCredentials: typeof fetch = (input, init) => {
+  return fetch(input, {
+    ...init,
+    credentials: 'include',
+  });
+};
+
 const baseURL = 'https://api.dev.hearlers.com';
-const baseConfig = { baseURL };
+const baseConfig = { baseURL, fetch: fetchWithCredentials };
 
 export const api = {
   V1: new V1(baseConfig),
