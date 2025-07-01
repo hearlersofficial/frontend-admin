ca0325c351fd4a0b3f7c049d0ad68672fffe0ff1
hhjeee <yoonhj0315@gmail.com>
2025-07-01 22:30:14 +0900

    [feat] add vite proxy configuration

--- app/api/index.ts
+++ app/api/index.ts
@@ -7,7 +7,13 @@ const fetchWithCredentials: typeof fetch = (input, init) => {
   });
 };
 
-const baseURL = 'https://api.dev.hearlers.com';
+// const baseURL = 'https://api.dev.hearlers.com';
+
+const isBrowser = typeof window !== 'undefined';
+const baseURL = isBrowser
+  ? '/api' // 브라우저 -> vite 프록시 경유
+  : 'https://api.dev.hearlers.com'; // 서버
+
 const baseConfig = { baseURL, fetch: fetchWithCredentials };
 
 export const api = {
