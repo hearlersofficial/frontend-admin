eee6eacbed194a3384621ff48b1daa250d4d849d
sjunee2 <tx0203@gmail.com>
2025-08-07 20:04:37 +0900

    feat: root page ssr 제거

--- app/root.tsx
+++ app/root.tsx
@@ -1,34 +1,9 @@
-import { json, Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteLoaderData } from '@remix-run/react';
-import type { LinksFunction, LoaderFunction } from '@remix-run/node';
+import { Links, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './tailwind.css';
 
-export const loader: LoaderFunction = async () => {
-  return json({
-    ENV: {
-      ENVIRONMENT: process.env.ENVIRONMENT,
-      BASE_URL: process.env.BASE_URL,
-      API_URL: process.env.API_URL,
-    },
-  });
-};
-
-export const links: LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
-  const data = useRouteLoaderData<typeof loader>('root');
   return (
     <html lang="en">
       <head>
@@ -36,15 +11,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet" />
       </head>
       <body>
         {children}
         <ScrollRestoration />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.ENV = ${JSON.stringify(data.ENV || {})};`,
-          }}
-        />
         <Scripts />
       </body>
     </html>
@@ -52,7 +25,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  // 리로드시 새 인스턴스 생성을 방지하기 위해 remember or useState 사용예정
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
