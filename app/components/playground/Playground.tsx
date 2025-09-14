04f408c03d700c517df9d9f060a815944e923d8f
astor-dev <orangnlp@gmail.com>
2025-09-14 15:46:39 +0900

    feat: add mobile preview button

--- app/components/playground/Playground.tsx
+++ app/components/playground/Playground.tsx
@@ -23,15 +23,14 @@ const Playground = () => {
   }, [temporaryVersionData, setTemporaryVersion]);
 
   return (
-    <div className="flex h-full w-full overflow-hidden">
+    <div className="relative flex h-full w-full overflow-hidden">
       <Sidebar />
-      <div className="flex w-full flex-1 overflow-hidden rounded-xl rounded-tl-none bg-white px-8 py-6">
+      <div className="flex w-full flex-1 flex-col overflow-hidden rounded-xl rounded-tl-none bg-white px-8 py-6">
         <div className="flex w-full flex-1 flex-col gap-6 overflow-hidden">
           <Technique key={`technique-${temporaryVersion?.id}`} />
           <Prompt key={`prompt-${temporaryVersion?.id}`} />
         </div>
       </div>
-      <div className="ml-6 flex items-start overflow-hidden">{/* <MobilePreview /> */}</div>
     </div>
   );
 };
